714f3c04f40b460dff3fc908db78a2d411d8b929
wwwam <amannakoti0@gmail.com>
2026-09-02 18:12:03 +0530

    porject module installed

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//mongooooose
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MONGO DB BROOO"))
+  .catch((err) => console.error("MONGO Connection Err:", err));
+
+//api test
+app.get("/api/health", (req, res) => {
+  res.json({
+    status: "ok",
+    timestamp: new Date(),
+    message: "api is Running Smoothly...",
+  });
+});
+//server start
+app.listen(PORT, () => {
+  console.log(`Server is running on this port ${PORT}`);
+});
